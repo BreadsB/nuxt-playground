5f26b8daba1f14238c9ba67b7469984a4afd5bf0
Michael <102490720+BreadsB@users.noreply.github.com>
2024-11-07 16:17:23 +0100

    Mobile menu 1 - Nuxt
    Menu that is comming from left side on whole screen and showing bunch of links on center.

---
+++ Nuxt-MobileMenu-1/app.vue
@@ -0,0 +1,33 @@
+<template>
+  <MenuComponent />
+  <!-- <HomeComponent /> -->
+</template>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Lato', sans-serif;
+  font-family: 'Oswald', sans-serif;
+}
+
+.content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: -1;
+  text-align: center;
+  width: 100%;
+  color: #202020;
+}
+.content .title {
+  font-size: 40px;
+  font-weight: 700;
+}
+.content p {
+  font-size: 35px;
+  font-weight: 600;
+}
+</style>
---
+++ Nuxt-MobileMenu-1/components/HomeComponent.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="content">
+    <div class="title">Fullscreen Overlay Navigation Bar</div>
+    <p>(Hamburger Menu-2)</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
